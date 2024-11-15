47cce20164de8010e537a523a95df069e0fd09d3
yeo-won <yw112912@dgsw.hs.kr>
2024-11-15 13:32:58 +0900

    ⚡️ :: onboarding page 개발
    버튼 기능 구현

--- app/src/main/java/com/example/kong_android/MainActivity.kt
+++ app/src/main/java/com/example/kong_android/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.example.kong_android
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
+import com.example.kong_android.auth.LoginActivity
+import com.example.kong_android.auth.SignupActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton: AppCompatButton = findViewById(R.id.login_btn)
+        val signupButton: AppCompatButton = findViewById(R.id.signup_btn)
+
+        // 로그인 버튼 클릭 시 로그인 페이지로 이동
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 회원가입 버튼 클릭 시 회원가입 페이지로 이동
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
