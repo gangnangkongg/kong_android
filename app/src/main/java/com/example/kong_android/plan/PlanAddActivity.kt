7540554a39762b7e9167e3f9d9533b2b6f6b1046
yeo-won <yw112912@dgsw.hs.kr>
2024-11-24 22:31:55 +0900

    ⚡️ :: Plan Add page view 개발
    계획 등록 페이지 구현

--- app/src/main/java/com/example/kong_android/plan/PlanAddActivity.kt
+++ app/src/main/java/com/example/kong_android/plan/PlanAddActivity.kt
@@ -1,4 +1,33 @@
 package com.example.kong_android.plan
 
-class PlanAddActivity {
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.example.kong_android.R
+
+class PlanAddActivity: AppCompatActivity() {
+    private lateinit var startDateEdit: EditText
+    private lateinit var endDateEdit: EditText
+    private lateinit var targetAmountEdit: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plan_add)
+
+        startDateEdit = findViewById(R.id.start_date_edit)
+        endDateEdit = findViewById(R.id.end_date_edit)
+        targetAmountEdit = findViewById(R.id.target_amount_edit)
+
+        val backButton: ImageButton = findViewById(R.id.back_btn)
+        val planAddBtn: AppCompatButton = findViewById(R.id.plan_add_btn)
+
+        /* 뒤록가기 버튼 클릭 시 현재 페이지 종료 */
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        /* 등록하기 버튼 클릭 시 서버에 전송 */
+    }
 }
