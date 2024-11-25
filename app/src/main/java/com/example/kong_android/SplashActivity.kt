4a784d2ab4f4d7f5b1ad7aea9b0ab5aceb1d0199
yeo-won <yw112912@dgsw.hs.kr>
2024-11-25 14:17:21 +0900

    ⚡️ :: Splash page 개발
    splash 화면 구현

---
+++ app/src/main/java/com/example/kong_android/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kong_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 3초 후 메인 액티비티로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
