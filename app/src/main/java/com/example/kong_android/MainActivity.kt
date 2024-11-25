fae79f6a4bb6e0a787d90321efb8a76b05f8e967
yeo-won <yw112912@dgsw.hs.kr>
2024-11-25 15:13:23 +0900

    ⚡️ :: onboarding page 추가 구현
    온보딩 페이지 버튼 추가

--- app/src/main/java/com/example/kong_android/MainActivity.kt
+++ app/src/main/java/com/example/kong_android/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kong_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.example.kong_android.auth.LoginActivity
@@ -14,6 +16,17 @@ class MainActivity : AppCompatActivity() {
 
         val loginButton: AppCompatButton = findViewById(R.id.login_btn)
         val signupButton: AppCompatButton = findViewById(R.id.signup_btn)
+        val pingButton: AppCompatButton = findViewById(R.id.ping_btn)
+        val pingImage: ImageView = findViewById(R.id.ping_img)
+
+        // 로고 중앙 버튼 클릭 시 이미지 출력
+        pingButton.setOnClickListener {
+            if (pingImage.visibility == View.GONE) {
+                pingImage.visibility = View.VISIBLE
+            } else {
+                pingImage.visibility = View.GONE
+            }
+        }
 
         // 로그인 버튼 클릭 시 로그인 페이지로 이동
         loginButton.setOnClickListener {
